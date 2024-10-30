79340b3a36597aef4b40a49ce0767cd13f373ae5
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-10-30 00:22:59 -0400

    :sparkles: feat: Header-uses-location+Home-grid-and-css

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { FaBell, FaUserCircle } from 'react-icons/fa';
+import { FaUserCircle } from 'react-icons/fa';
+import { MdNotificationsNone } from 'react-icons/md';
+import { useLocation } from 'react-router-dom';
 import '../assets/styles/Header.css';
+
+const capitalize = (str: string) => {
+    return str.charAt(1).toUpperCase() + str.slice(2);
+};
+
 const Header: React.FC = () => {
+    const location = useLocation();
+    const currentRouteName = location.pathname === '/' ? 'Home' : capitalize(location.pathname);
+
     return (
         <header className="header">
-            <div className="header-title">Home</div>
+            {currentRouteName}
             <div className="header-icons">
-                <FaBell className="icon" />
+                <MdNotificationsNone className="icon" />
                 <FaUserCircle className="icon" />
             </div>
         </header>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,9 +4,27 @@ import '../assets/styles/Home.css';
 const Home: React.FC = () => {
     return (
         <div className="home-container">
-            Hello
+            <div className="column">
+                <div className="row-content bg-danger d-flex align-items-center justify-content-center">
+                    First Row
+                </div>
+                <div className="row-content bg-success d-flex align-items-center justify-content-center">
+                    Second Row
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="row-content bg-danger d-flex align-items-center justify-content-center">
+                    First Row
+                </div>
+                <div className="row-content bg-success d-flex align-items-center justify-content-center">
+                    Second Row
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Home;
+
+
